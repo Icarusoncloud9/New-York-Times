c727902c4b7cdb88d3503bc55da75e248c9c8027
Your name <Melarajoaquin@gmail.com>
2016-10-17 00:39:39 -0400

    It is finally working in the backend!

--- assets/javascript/nyt.js
+++ assets/javascript/nyt.js
@@ -1,51 +1,204 @@
-// Declared Variables ----------------------------------------------------
-// Search bars
+// =======================================================================
+// Declared Global Variables
+// =======================================================================
 
-var search = $("#search").val();
-var number = $("#number").val();
-var start = $("#start").val();
-var end = $("#end").val();
+var search;
 
-// Buttons
+var sort = "";
+
+var start;
+
+var end;
+
+
+// =======================================================================
+// Buttons from html document
+// =======================================================================
 
 var searchButton = $("#searchButton");
+
 var clearResults = $("#clearResults");
 
+
+// =======================================================================
 // Url of website
+// =======================================================================
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+var website = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
-// Functions -------------------------------------------------------------
 
+// =======================================================================
+// Functions 
+// =======================================================================
+
+// This enables the dropdown menu for the search selection of the New York Times
+$('.dropdown-toggle').dropdown();
+
+// This is a function that changes the displayed value of the dropdown menu
+$(function(){
+
+	$(".dropdown-menu li a").click(function() {
+
+		$(".dropdown-toggle:first-child").text($(this).text());
+
+		$(".dropdown-toggle:first-child").val($(this).text());
+	
+	})
+
+});
+
+// Dynamically displaying things with the JSON info
+function create(result) {
+
+	$(".resultsDisplayed").html("");
+
+	for(var i = 0; i < 10; i++) {
+
+		var articleDiv = $("<div>");
+
+		var articleLink = $("<a>");
+
+			articleLink.attr("href", result.response.docs[i].web_url);
+
+			articleLink.attr("class", "list-group-item list-group-item-action articleHeading");
+
+		var heading = $("<h5>");
+
+			heading.attr("class", "list-group-item-heading");
+
+			heading.text(result.response.docs[i].headline.main);
+
+		var articleText = $("<p>");
+
+			articleText.attr("class", "list-group-item-text articleText");
+
+			articleText.text(result.response.docs[i].snippet);
+
+		articleLink.append(heading);
+
+		articleLink.append(articleText);
+
+		articleDiv.append(articleLink);
+
+		$(".resultsDisplayed").append(articleDiv);
+
+	}
+
+}
+
+// Ajax call on click of the searchButton
 searchButton.on("click", function() {
- 	url += '?' + search + '?' + 5 + '?' + start;
+	
+		search = $("#search").val();
+
+		sort = $("#sortitem").val();
+
+		console.log( "Line 57", sort );
+		
+		start = $("#start").val();
+		
+		end = $("#end").val();
+
+		if(search != "") {
+
+			website += '?' + $.param({
+
+	 		'api-key': "2a966230874544079d3f4400ec3ef469",
+
+	 		'q': search,
+
+	 		'sort': sort
+
+	 		});
+
+		} else if (start != "") {
+
+			website += '?' + $.param({
+
+	 		'api-key': "2a966230874544079d3f4400ec3ef469",
+
+	 		'q': search,
+
+	 		'sort': sort,
+
+	 		'begin_date': start
+
+			});
+
+		} else if (end != "") {
+
+			website += '?' + $.param({
+
+	 		'api-key': "2a966230874544079d3f4400ec3ef469",
+
+	 		'q': search,
+
+	 		'sort': sort,
+
+	 		'end_date': end
+
+			});
+
+		} else if (start != "" && end != "") {
+
+			website += '?' + $.param({
+
+	 		'api-key': "2a966230874544079d3f4400ec3ef469",
+
+	 		'q': search,
+
+	 		'sort': sort,
+
+	 		'begin_date': start,
+
+	 		'end_date': end
+
+			});
+
+		} else if (search.length > 0) {
+
+			alert("The search bar is empty!");
+
+		}
+	
+ 	console.log(website);
+
  	$.ajax({
-	  url: url,
+
+	  url: website,
+
 	  method: 'GET',
-	}).done(function(result) {
-	  console.log(result.response);
-	}).fail(function(err) {
-	  throw err;
+
+	}).done(function( result ) {
+
+	// console.log(result.response);
+	// console.log(result.response.docs);
+	// console.log(result.response.docs[0]);
+	// console.log(result.response.docs[0].headline.main);
+	// console.log(result.response.docs[0].web_url);
+	// console.log(result.response.docs[0].snippet);
+
+	create(result);
+
+	}).fail(function( err ) {
+
+		console.log("error");
+
+		throw err;
+
 	});
 
-
 });
 
+// Clearing the value of each thing in the search bar
 clearResults.on("click", function() {
+
 	$("#search").val('');
-	$("#number").val('');
+
 	$("start").val('');
+
 	$("end").val('');
+
 });
-
-
-
-
-// Main code
-
-
-// Getting the Json info
-
-
 
 
